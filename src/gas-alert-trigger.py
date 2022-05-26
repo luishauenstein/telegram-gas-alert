72eee66239fff985d061c39aa4c0619684988f68
Luis <account@luishauenstein.com>
2022-05-27 00:30:01 +0200

    implement alert trigger sql query

--- src/gas-alert-trigger.py
+++ src/gas-alert-trigger.py
@@ -1,5 +1,7 @@
+from operator import and_
 from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
+import time
 
 from schema import Alert
 
@@ -7,7 +9,20 @@ from schema import Alert
 postgres_url_string = "postgresql://postgres@localhost:5432/telegram-gas-alert"
 engine = create_engine(postgres_url_string, echo=False, future=True)
 
-stmt = select(Alert).where(Alert.cooldown_seconds <= 2000)
+
+current_timestamp = time.time()
+current_gas = 21  # example hardcoded value for now
+
+stmt = select(Alert).where(
+    and_(
+        Alert.cooldown_expired_timestamp <= current_timestamp,
+        Alert.gas_threshold_gwei <= current_gas,
+    )
+)
 with Session(engine) as session:
     for row in session.execute(stmt):
-        print(f"{row.Alert.alert_id} {row.Alert.cooldown_seconds}")
+        print(
+            f"{row.Alert.alert_id} {row.Alert.gas_threshold_gwei} {row.Alert.cooldown_expired_timestamp}"
+        )
+        # 1. trigger alert
+        # 2. update cooldown_expired_timestamp

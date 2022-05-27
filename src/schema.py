1f50b02871b99225e23c351f57f72054b206a8ec
Luis <account@luishauenstein.com>
2022-05-27 11:39:46 +0200

    switch telegram_chat_id and alert_id column data types from in to bigint

--- src/schema.py
+++ src/schema.py
@@ -12,8 +12,8 @@ Base = declarative_base()
 
 class Alert(Base):
     __tablename__ = "active_alerts"
-    alert_id = Column(Integer, primary_key=True)
-    telegram_chat_id = Column(Integer, nullable=False)
+    alert_id = Column(BigInteger, primary_key=True)
+    telegram_chat_id = Column(BigInteger, nullable=False)
     gas_threshold_gwei = Column(Integer, nullable=False)
     cooldown_seconds = Column(Integer, nullable=False)
     cooldown_expired_timestamp = Column(BigInteger, nullable=False)
